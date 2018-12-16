a2ae3172eb0c348572f776a8e36f78d913553cd3
Steven Yan <33998332+Steven-nagisa-Y@users.noreply.github.com>
2018-12-16 20:25:10 +0800

    New add  base64

--- $hasher.py
+++ $hasher.py
@@ -3,6 +3,7 @@
 import hashlib
 import os
 import time
+import base64
 
 def writefile(text):
 #	print(text)
@@ -12,34 +13,87 @@ def hasher(files):
 	hasher2 = hashlib.sha1()
 	hasher3 = hashlib.sha256()
 	#file_name = input('Input a filename:')
-	with open(files,'rb') as afile:
-		infile = afile.read()
-		hasher1.update(infile)
-		hasher2.update(infile)
-		hasher3.update(infile)
+	with open(files,'rb') as fileOj1:
+		infile1 = fileOj1.read()
+		hasher1.update(infile1)
+		hasher2.update(infile1)
+		hasher3.update(infile1)
 		writefile('The md5 of '+files+' is:'+hasher1.hexdigest())
 		writefile('The sha1 of '+files+' is:'+hasher2.hexdigest())
 		writefile('The sha256 of '+files+' is:'+hasher3.hexdigest())
 		writefile('\n')
+		
+def base64encode(files):
+	with open(files,'rb') as fileOj2:
+		infile2 = fileOj2.read()
+		b64encode = base64.b64encode(infile2)
+		writefile('The base64 of '+files+' is: '+str(b64encode))
+		writefile('\n')
+	
 def getListFiles(path):
 	ret = [] 
 	for root, dirs, files in os.walk(path):
 		for filespath in files:
 			return files
+	
+def getDirsFiles(path):
+	ret = [] 
+	for root, dirs, files in os.walk(path):
+		for filespath in files:
+			ret.append(os.path.join(root,filespath))
+	return ret
 
 if __name__ == '__main__':
-	print('Progressing...\n')
-	f = open('hashlist.txt','w+')
-	start = time.clock()
-	writefile('########## The Beginning of File ##########\n')
-	writefile('The progressing path is: '+os.getcwd())
-	writefile('There are(is) '+str(len(getListFiles('./')))+' item(s)\n')
-	for i in getListFiles('./'):
-		writefile('This is '+i)
-		writefile('File size:%.3f'%(os.path.getsize(i)/1024/1024)+'MB')
-		hasher(i)
-	end = time.clock()
-	writefile('Total time consuming：'+str(end-start)+'s\n')
-	print('Done.\n')
-	print('Total time consuming：'+str(end-start)+'s\n')
-	writefile('\n########## The End of File ##########')
+	print('What do you want to do?')
+	toDo = input('Enter \'hash\' to calc hash of files.\nEnter \'base64\' to calc base64 of files.\n>')
+	toGet = input('Calc all files in dirs?(y/n)\n>')
+	if toGet == 'y':
+		s_toget = True
+	else:
+		s_toget = False
+	if toDo == 'hash':
+		print('Progressing...\n')
+		f = open('hashlist.txt','w+')
+		start = time.clock()
+		writefile('########## The Beginning of File ##########\n')
+		writefile('The progressing path is: '+os.getcwd())
+		writefile('There are(is) '+str(len(getListFiles('./')))+' item(s)\n')
+		if s_toget:	
+			for i in getDirsFiles('./'):
+				writefile('This is '+i)
+				writefile('File size:%.3f'%(os.path.getsize(i)/1024/1024)+'MB')
+				hasher(i)
+		else:
+			for i in getListFiles('./'):
+				writefile('This is '+i)
+				writefile('File size:%.3f'%(os.path.getsize(i)/1024/1024)+'MB')
+				hasher(i)
+		end = time.clock()
+		writefile('Total time consuming：'+str(end-start)+'s\n')
+		print('Done.\n')
+		print('Total time consuming：'+str(end-start)+'s\n')
+		writefile('\n########## The End of File ##########')
+	elif toDo == 'base64':
+		print('Progressing...\n')
+		f = open('base64list.txt','w+')
+		start = time.clock()
+		writefile('########## The Beginning of File ##########\n')
+		writefile('The progressing path is: '+os.getcwd())
+		writefile('There are(is) '+str(len(getListFiles('./')))+' item(s)\n')
+		if s_toget:
+			for i in getDirsFiles('./'):
+				writefile('This is '+i)
+				writefile('File size:%.3f'%(os.path.getsize(i)/1024/1024)+'MB')
+				base64encode(i)
+		else:
+			for i in getListFiles('./'):
+				writefile('This is '+i)
+				writefile('File size:%.3f'%(os.path.getsize(i)/1024/1024)+'MB')
+				base64encode(i)
+		end = time.clock()
+		writefile('Total time consuming：'+str(end-start)+'s\n')
+		print('Done.\n')
+		print('Total time consuming：'+str(end-start)+'s\n')
+		writefile('\n########## The End of File ##########')
+	else:
+		quit()
